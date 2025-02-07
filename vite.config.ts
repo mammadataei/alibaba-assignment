9e8ad22b7f76cc30f72efe7ac33371bc9a487c6e
Mohammad Ataei <mammadataei@gmail.com>
2025-02-07 21:07:55 +0330

    chore: setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { pluginJsonServer } from 'vite-plugin-json-server'
 import RemixRouter from 'vite-plugin-remix-router'
 
@@ -8,6 +9,29 @@ import RemixRouter from 'vite-plugin-remix-router'
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Hotel Booking',
+        short_name: 'Hotel Booking',
+        description: 'Alibaba Hotel Booking App',
+        theme_color: '#ffffff',
+        icons: [
+          // Customize the icons for PWA
+          // { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          // { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          // ...
+        ],
+      },
+      workbox: {
+        sourcemap: true,
+        cleanupOutdatedCaches: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
     pluginJsonServer({
       apiPath: '/api',
       profile: './server',
